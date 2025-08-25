57a49d2071c5fff3a7dec4f0cd42e90bc943abde
Dragan Milanović <dragan2dix@gmail.com>
2025-08-25 17:41:08 +0200

    Add files via upload

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -42,13 +42,11 @@ export function Card({
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
+      <div className="flex p-3">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p
-        className={`truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
+      <p className={`truncate rounded-xl bg-white py-8 text-center text-2xl`}>
         {value}
       </p>
     </div>
