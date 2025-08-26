91d781266ee390464fad0de4bf4182698e3b19bc
Dragan Milanović <dragan2dix@gmail.com>
2025-08-26 15:32:49 +0200

    Add files via upload

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,8 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import DXLogo from "../dx-logo";
 
 export default function SideNav() {
   return (
@@ -11,7 +12,8 @@ export default function SideNav() {
         href="/"
       >
         <div className="text-white w-40">
-          <AcmeLogo />
+          {/* <AcmeLogo /> */}
+          <DXLogo />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
