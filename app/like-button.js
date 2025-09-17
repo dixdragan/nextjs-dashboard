2dc65daf5ccd29922dfb7f66d55dbe72413b64bc
Dragan Milanović <dragan2dix@gmail.com>
2025-09-17 11:00:10 +0200

    Add files via upload

--- app/like-button.js
+++ app/like-button.js
@@ -1,6 +1,8 @@
 "use client";
 
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 export default function LikeButton() {
   const [likes, setLikes] = useState(0);
@@ -9,5 +11,12 @@ export default function LikeButton() {
     setLikes(likes + 1);
   }
 
-  return <button onClick={handleClick}>Like ({likes})</button>;
+  return (
+    <Button
+      onClick={handleClick}
+      className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+    >
+      Like <HandThumbUpIcon className="w-5 md:w-6" /> ({likes})
+    </Button>
+  );
 }
